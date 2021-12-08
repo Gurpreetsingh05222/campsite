7a90284af7e1c10346344c2728de80c63a3bd8f6
gurpreetsingh05222 <gurpreetsingh05222@gmail.com>
2021-12-08 15:56:39 -0500

    map cluster and homepage design

--- models/campsite.js
+++ models/campsite.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+const opts = { toJSON: {virtuals: true }};
+
 const CampsiteSchema = new Schema({
     title: String,
     images: [
@@ -34,6 +36,11 @@ const CampsiteSchema = new Schema({
             ref: 'Review'
         }
     ]
+}, opts);
+
+CampsiteSchema.virtual('properties.popUpMarkup').get(function() {
+    return `<strong><a href="/campsites/${this._id}">${this.title}</a></strong>
+    <p>${this.description.substring(0, 20)}...</p>`;
 });
 
 //delete the reviews related to campsite after it is deleted
